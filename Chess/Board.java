11fa967326951266a46be8247ce0c225bf51ee9d
jds-472 <jds472@scarletmail.rutgers.edu>
2025-02-25 03:05:47 +0000

    created an initialization for board and getpieces

--- Chess/Board.java
+++ Chess/Board.java
@@ -1,35 +1,48 @@
 package Chess;
 
+import java.util.ArrayList;
+
 public class Board {
-    public static final boolean WHITE = true;
-    public static final boolean BLACK = false;
-    private Piece[][] board;
+    private static Piece[][] board;
 
     public Board() {
         board = new Piece[8][8];
     }
-    public void initialize() {
+    public static void initialize() {
+        board = new Piece[8][8];
         // Initialize pieces on the board
         for (int i = 0; i < 8; i++) {
-            board[1][i] = new Pawn(WHITE);
-            board[6][i] = new Pawn(BLACK);
+            board[1][i] = new Pawn(Chess.Player.white, ReturnPiece.PieceType.WP, ReturnPiece.PieceFile.values()[i], 2);
+            board[6][i] = new Pawn(Chess.Player.black, ReturnPiece.PieceType.BP, ReturnPiece.PieceFile.values()[i], 7);
         }
-        board[0][0] = new Rook(WHITE);
-        board[0][1] = new Knight(WHITE);
-        board[0][2] = new Bishop(WHITE);
-        board[0][3] = new Queen(WHITE);
-        board[0][4] = new King(WHITE);
-        board[0][5] = new Bishop(WHITE);
-        board[0][6] = new Knight(WHITE);
-        board[0][7] = new Rook(WHITE);
+        board[0][0] = new Rook(Chess.Player.white, ReturnPiece.PieceType.WR, ReturnPiece.PieceFile.a, 1);
+        board[0][1] = new Knight(Chess.Player.white, ReturnPiece.PieceType.WN, ReturnPiece.PieceFile.b, 1);
+        board[0][2] = new Bishop(Chess.Player.white, ReturnPiece.PieceType.WB, ReturnPiece.PieceFile.c, 1);
+        board[0][3] = new Queen(Chess.Player.white, ReturnPiece.PieceType.WQ, ReturnPiece.PieceFile.d, 1);
+        board[0][4] = new King(Chess.Player.white, ReturnPiece.PieceType.WK, ReturnPiece.PieceFile.e, 1);
+        board[0][5] = new Bishop(Chess.Player.white, ReturnPiece.PieceType.WB, ReturnPiece.PieceFile.f, 1);
+        board[0][6] = new Knight(Chess.Player.white, ReturnPiece.PieceType.WN, ReturnPiece.PieceFile.g, 1);
+        board[0][7] = new Rook(Chess.Player.white, ReturnPiece.PieceType.WR, ReturnPiece.PieceFile.h, 1);
 
-        board[7][0] = new Rook(BLACK);
-        board[7][1] = new Knight(BLACK);
-        board[7][2] = new Bishop(BLACK);
-        board[7][3] = new Queen(BLACK);
-        board[7][4] = new King(BLACK);
-        board[7][5] = new Bishop(BLACK);
-        board[7][6] = new Knight(BLACK);
-        board[7][7] = new Rook(BLACK);
+        board[7][0] = new Rook(Chess.Player.black, ReturnPiece.PieceType.BR, ReturnPiece.PieceFile.a, 8);
+        board[7][1] = new Knight(Chess.Player.black, ReturnPiece.PieceType.BN, ReturnPiece.PieceFile.b, 8);
+        board[7][2] = new Bishop(Chess.Player.black, ReturnPiece.PieceType.BB, ReturnPiece.PieceFile.c, 8);
+        board[7][3] = new Queen(Chess.Player.black, ReturnPiece.PieceType.BQ, ReturnPiece.PieceFile.d, 8);
+        board[7][4] = new King(Chess.Player.black, ReturnPiece.PieceType.BK, ReturnPiece.PieceFile.e, 8);
+        board[7][5] = new Bishop(Chess.Player.black, ReturnPiece.PieceType.BB, ReturnPiece.PieceFile.f, 8);
+        board[7][6] = new Knight(Chess.Player.black, ReturnPiece.PieceType.BN, ReturnPiece.PieceFile.g, 8);
+        board[7][7] = new Rook(Chess.Player.black, ReturnPiece.PieceType.BR, ReturnPiece.PieceFile.h, 8);
+    }
+
+    public static ArrayList<ReturnPiece> getPieces() {
+        ArrayList<ReturnPiece> pieces = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] != null) {
+                    pieces.add(board[i][j]);
+                }
+            }
+        }
+        return pieces;
     }
 }
